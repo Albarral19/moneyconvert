acfde32052233466e8bbc98ee1caee296854a63d
Albarral19 <jalbarral96@gmail.com>
2020-09-28 20:46:56 -0300

    started refactoring the site with Materialize and add scss file

--- js/index.js
+++ js/index.js
@@ -1,30 +1,8 @@
-//#region Cotação do Dólar
-    var url_cotacao = "https://economia.awesomeapi.com.br/all/USD-BRL";
-    var dolarHoje;
-    let request = new XMLHttpRequest();
-    request.open('GET', url_cotacao);
-    request.send();
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+  });
 
-    request.onload = function(){
-        dolarHoje = JSON.parse(request.responseText).USD.bid;
-        let cotacao = document.querySelector('#cotacao');
-        cotacao.innerHTML = dolarHoje;
-        document.getElementById("real").value = dolarHoje;       
-    };
-//#endregion
-
-    function dolarReal()
-    {
-        var input = document.getElementById("dolar");
-        input.value = input.value.replace(/[a-zA-Z]/g,'');
-
-        document.getElementById("real").value = (document.getElementById("dolar").value * dolarHoje).toFixed(2);    
-    }
-
-    function realDolar() 
-    {
-        var input = document.getElementById("real");
-        input.value = input.value.replace(/[a-zA-Z]/g,'');
-
-        document.getElementById("dolar").value = (document.getElementById("real").value / dolarHoje).toFixed(2);     
-    }
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
