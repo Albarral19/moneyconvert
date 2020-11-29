b48f1b5724554008b3b0dfae62fe012b6cffedab
José Albarral <jalbarral96@gmail.com>
2020-11-29 17:27:11 -0300

    finishing contact styling

--- contact.php
+++ contact.php
@@ -10,15 +10,16 @@
 <body>
     <div class="container">
         <div class="row justify-content-center">
-            <h1 class= "col-12 text-center">Send Mail</h1>
+            <h1 class= "col-12 text-center">Contact Us</h1>
             <div class="contato">
                 <form action="./email.php" class="form" method="POST">
                     <input class="field" name="name" placeholder="Name" autocomplete="off">
                     <input class="field" name="email" placeholder="E-Mail" autocomplete="off">
                     <textarea class="field" name="message" placeholder="Type your message here" autocomplete="off"></textarea>
-                    <input class="field" type="submit" value="Enviar">
+                    <input class="field" type="submit" value="Submit">                    
                 </form>
-            </div>
+                <a href="index.php"><input class="col-12 previous" type="submit" value="Previous"></a>                  
+            </div>                     
         </div>        
     </div>    
     <?php include'shared/js.php'?>
--- email.php
+++ email.php
@@ -1,45 +1,22 @@
 <?php
-require 'mailer/PHPMailer.php';
 
-if (isset($_POST['name']) && !empty($_POST['name'])){
-    $nome = $_POST['name'];
-}
+    $nome     = isset($_POST['name'])     ? $_POST['name']     : '';
+    $email    = isset($_POST['email'])    ? $_POST['email']    : '';
+    $message = isset($_POST['message']) ? $_POST['message'] : '';
 
-if (isset($_POST['email']) && !empty($_POST['email'])){
-    $email = $_POST['email'];
-}
-
-if (isset($_POST['message']) && !empty($_POST['message'])){
-    $message = $_POST['message'];
-}
-
-$mail = new PHPMailer();
-
-$mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls';
-$mail->Username = 'usuariomoneyconvert@gmail.com';
-$mail->Password = 'moneyconvert123#';
-$mail->Port = 587;
-
-//https://temp-mail.org/pt/
-//variavel onde é inserido o email do destinatário
-$from = "kefacec762@58as.com";
-
-$mail->setFrom($from, 'Contato');
-$mail->addAddress('usuariomoneyconvert@gmail.com');
-
-$mail->isHTML(true);
-
-$mail->Subject = 'ContatoUsuário - MoneyConvert';
-$mail->Body    = 'Email: '. $email . '\nMensagem: ' . $mensagem;
-
-if(!$mail->send()) {
-    echo 'Não foi possível enviar a mensagem.<br>';
-    echo 'Erro: ' . $mail->ErrorInfo;
-} else {
-    echo 'Mensagem enviada.';
-}
-
+    if ($nome && $email && $message) {
+        ?>
+            <div class="alert alert-success">
+                Dados enviados com sucesso!
+            </div>
+        <?php
+    }
+    else {
+        ?>
+            <div class="alert alert-warning">
+                Os dados de contato precisam ser preenchidos!
+            </div>
+        <?php                
+    }    
+    include 'contact.php';
 ?>
