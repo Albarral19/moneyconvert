53dd3fa1d513735ede70602464060cd3b37d0f5f
Albarral19 <jalbarral96@gmail.com>
2020-12-02 02:11:35 -0300

    finalizando a pagina de contato

--- email.php
+++ email.php
@@ -1,15 +1,52 @@
 <?php
 
-    $nome     = isset($_POST['name'])     ? $_POST['name']     : '';
-    $email    = isset($_POST['email'])    ? $_POST['email']    : '';
-    $message = isset($_POST['message']) ? $_POST['message'] : '';
+    $name     = isset($_POST['name'])     ? utf8_decode($_POST['name'])     : '';
+    $email    = isset($_POST['email'])    ? utf8_decode($_POST['email'])    : '';
+    $message = isset($_POST['message']) ? utf8_decode($_POST['message']) : '';
 
-    if ($nome && $email && $message) {
-        ?>
-            <div class="alert alert-success">
-                Dados enviados com sucesso!
-            </div>
-        <?php
+    if ($name && $email && $message && filter_var($email, FILTER_VALIDATE_EMAIL)) { 
+        try{            
+            $to = "email@email.com"; //destinatario
+            $messageSubject = "Contact - Money Convert";
+            $body = "";
+
+            $body .= "From: ".$name. "\r\n";
+            $body .= "Email: ".$email. "\r\n";
+            $body .= "Message: ".$message. "\r\n";       
+
+            //Request PHPMailer
+            require 'mailer/PHPMailerAutoload.php';
+            $mail = new PHPMailer;
+            $mail->isSMTP();
+            
+            //cfg mail server
+            $mail->Host = "smtp.gmail.com";       
+            $mail->Port = "587";
+            $mail->SMTPSecure = "tls";
+            $mail->SMTPAuth = "true";
+            $mail->Username = "testephpMC@gmail.com";
+            $mail->Password = "phpatequeebom";
+
+            //cfg message
+            $mail->setFrom($mail->Username, $name); // Remetente
+            $mail->addAddress($to); // Destinatário
+            $mail->Subject = $messageSubject;
+            $mail->Body = $body;
+            $mail->send();
+            
+            ?>
+                <div class="alert alert-success">
+                    Dados enviados com sucesso!
+                </div>
+            <?php
+            
+        }catch (Exception $e){
+            ?>
+                <div class="alert alert-warning">
+                    Falha ao enviar o e-mail: <?php $mail->ErrorInfo; ?>
+                </div>
+            <?php
+        } 
     }
     else {
         ?>
